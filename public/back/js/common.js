a12cd93fb00b5d660eff5011d4cc71ac8b2ab5b9
caichuanlu <2110572225@qq.com>
2017-10-30 14:48:55 +0800

    完成登录页面和管理界面首页

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,77 @@
 /**
  * Created by JIANG on 2017/10/29.
  */
+
+//首先要判断用户是否登录
+if(location.href.indexOf("login.html")<0){
+  $.ajax({
+    type:"get",
+    url:"/employee/checkRootLogin",
+    success:function(data){
+      if(data===400){
+        location.href="login.html";
+      }
+    }
+  })
+}
+
+
+//进度条功能
+$(function(){
+  //进度条功能
+  $(document).ajaxStart(function(){
+    NProgress.start();
+  });
+  
+  $(document).ajaxStop(function(){
+    setTimeout(function(){
+      NProgress.done();
+    },500);
+  })
+  
+  
+  //头部菜单按钮动画
+  $(".justify").on("click",function(){
+    $(".aside").toggleClass("now");
+    $(".contener").toggleClass("now");l
+  })
+  
+  //左侧导航动画开始
+  $(".nav li").on("click",function(){
+    $(this).toggleClass("now");
+  })
+  
+  //二级分类点击事件
+  $(".category").on("click",function(){
+    $(".child").slideToggle();
+  })
+  
+  
+  //公用退出功能
+    //1.显示模态框
+    $(".logout").on("click",function(){
+      $("#myModal").modal("show");
+    })
+    //2.点击模态框上的取消按钮,退出模态框模式
+    $(".cancel").on("click",function(){
+      $("#myModal").modal("hidden");
+    })
+    //3.点击模态框的确认按钮,确认退出
+    $(".confirm").on("click",function(){
+      $.ajax({
+        type:"get",
+        url:"/employee/employeeLogout",
+        success:function(data){
+          //如果退出成功,则跳转到登录界面
+          if(data.success){
+            location.href="login.html";
+          }
+        }
+      })
+    })
+})
+
+
+
+
+
--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,3 +1,91 @@
-/**
- * Created by JIANG on 2017/10/29.
- */
+$(function(){
+  //表单校验功能
+    //1.用户名,密码不能为空
+    //2.用户密码必须是6-12位
+  
+  
+  //初始化表单校验插件
+  var $form=$("#form");
+  $form.bootstrapValidator({
+    
+    //校验时使用的图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    
+    //配置校验规则
+    fields:{
+      //配置所有的字段的规则,对应表单中的name属性
+      username:{
+        validators:{
+          notEmpty:{
+            message:"用户名不能为空"
+          },
+          callback:{
+            message:"用户名错误"
+          }
+        }
+      },
+      password:{
+        validators:{
+          notEmpty:{
+            message:"用户密码不能为空"
+          },
+          stringLength:{
+            min:6,
+            max:12,
+            message:"用户密码必须是6-12位"
+          },
+          callback:{
+            message:"用户密码错误"
+          }
+        }
+      }
+    }
+  });
+  
+  //获取validator实例,通过该是实例调用一些方法
+  var validator=$("#form").data("bootstrapValidator");
+  
+  //注册表单成功提交事件
+  $form.on("success.form.bv",function(e){
+    //在表单提交的时候,默认会刷新页面,而通过ajax方式可以避免刷新
+    //所以要组织浏览器默认行为
+    e.preventDefault();
+    
+    //使用ajax方式提交数据
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$form.serialize(),
+      success:function(data){
+        if(data.success){
+          location.href="index.html"
+        }else{
+          if(data.error === 1000){
+            //使用js代码让username这个字段校验失败。
+            //第一个参数：name属性
+            //第二个参数：INVALID    VALID
+            //第三个参数：
+            validator.updateStatus("username", "INVALID", "callback");
+          }
+  
+          if(data.error === 1001){
+            validator.updateStatus("password", "INVALID", "callback");
+          }
+        }
+      }
+    })
+  });
+  
+  //表单重置功能
+  $("[type='reset']").on("click",function(){
+    validator.resetForm();
+  })
+  
+  
+  
+  
+});
